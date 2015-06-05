80e8246dda04b0540e765304aac8ef0c4bcf4952
AbhishekPandey1996 <prince.pandey127@gmail.com>
2015-06-05 12:34:26 +0530

    added Settings

--- TempMap/app/src/main/java/com/abhishek/tempmapproject/MainActivity.java
+++ TempMap/app/src/main/java/com/abhishek/tempmapproject/MainActivity.java
@@ -1,7 +1,98 @@
 package com.abhishek.tempmapproject;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 /**
  * Created by Abhishek on 05-06-2015.
  */
-public class MainActivity {
+public class MainActivity extends Fragment implements AdapterView.OnItemClickListener {
+    DrawerLayout drawer;
+    ListView lv;
+    String[] content;
+    MyAdapter myad;
+
+    View v;
+    private ActionBarDrawerToggle abdt;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.activity_main, container, false);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        lv = (ListView) v.findViewById(R.id.drawerlist);
+        content = v.getResources().getStringArray(R.array.itemslist);
+        myad = new MyAdapter(v.getContext());
+        lv.setAdapter(myad);
+        //lv.setAdapter(new ArrayAdapter<>(v.getContext(), android.R.layout.simple_list_item_1,content));
+        lv.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("Drawer Message", "Item " + position + " Clicked");
+        Toast.makeText(v.getContext(), "Item " + position + " Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+}
+
+class MyAdapter extends BaseAdapter {
+    Context context;
+    String[] content;
+    int[] picture = {android.R.drawable.ic_dialog_alert, android.R.drawable.ic_dialog_dialer, android.R.drawable.ic_dialog_email, android.R.drawable.ic_dialog_info, android.R.drawable.ic_dialog_map, android.R.drawable.btn_star};
+
+    public MyAdapter(Context context) {
+        this.context = context;
+        content = context.getResources().getStringArray(R.array.itemslist);
+    }
+
+    @Override
+    public int getCount() {
+        return content.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return content[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = null;
+        if (convertView == null) {
+            LayoutInflater inflating = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflating.inflate(R.layout.custom_nav_drawer, parent, false);
+
+        } else {
+            row = convertView;
+        }
+        TextView titletv = (TextView) row.findViewById(R.id.custumtv);
+        ImageView titleiv = (ImageView) row.findViewById(R.id.imageView1);
+        titletv.setText(content[position]);
+        titleiv.setImageResource(picture[position]);
+        return row;
+    }
 }
--- TempMap/app/src/main/java/com/abhishek/tempmapproject/MapsActivity.java
+++ TempMap/app/src/main/java/com/abhishek/tempmapproject/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.abhishek.tempmapproject;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
@@ -14,12 +16,19 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.io.IOException;
+import java.util.List;
 
+import android.location.Address;
+import android.location.Geocoder;
 public class MapsActivity extends FragmentActivity implements View.OnClickListener {
-
+    int counter=0;
+    double latitude;
+    double longitude;
     private GoogleMap mMap;
     LocationManager locm;
     String provider;
@@ -33,6 +42,72 @@ public class MapsActivity extends FragmentActivity implements View.OnClickListen
         setUpMapIfNeeded();
         b = (Button) findViewById(R.id.button2);
         b.setOnClickListener(this);
+
+       /* MainActivity fr = new MainActivity();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.tetslayout,fr,"asdf");
+        ft.commit();*/
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+       UiSettings mapSettings;
+        mapSettings = mMap.getUiSettings();
+        mapSettings.setZoomControlsEnabled(true);
+      mapSettings.setScrollGesturesEnabled(true);
+      mapSettings.setTiltGesturesEnabled(true);
+      mapSettings.setRotateGesturesEnabled(true);
+      mapSettings.setMyLocationButtonEnabled(true);
+
+
+        // List<Address> geocodeMatches = null;
+
+       /* try {
+            geocodeMatches =
+                    new Geocoder(MapsActivity.this).getFromLocationName(
+                            "Varanasi,India", 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("Checking", e.toString());
+        }
+
+        if (!geocodeMatches.isEmpty())
+        {
+            latitude = geocodeMatches.get(0).getLatitude();
+            longitude = geocodeMatches.get(0).getLongitude();
+        }
+*/
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(counter==0) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            counter =1;
+            Toast.makeText(this,"Satellite View Enabled",Toast.LENGTH_SHORT).show();
+        }
+    else if (counter==1){
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            counter =2;
+            Toast.makeText(this,"Normal View Enabled",Toast.LENGTH_SHORT).show();
+        }
+        else if (counter==2){
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            counter =3;
+            Toast.makeText(this,"Hybrid View Enabled",Toast.LENGTH_SHORT).show();
+        }
+        else if (counter==3){
+            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            counter =4;
+            Toast.makeText(this,"Terrain View Enabled",Toast.LENGTH_SHORT).show();
+        }
+        else if (counter==4){
+            mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+            counter =0;
+            Toast.makeText(this,"No View Enabled",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -55,16 +130,16 @@ public class MapsActivity extends FragmentActivity implements View.OnClickListen
 
     private void setUpMap() {
 
-       load.start();
-        if(latitu != 0) {
-            CameraPosition cam = new CameraPosition.Builder().target(new LatLng(latitu, longitu)).zoom(1).build();
+       /*load.start();
+        if(latitude != 0) {
+            CameraPosition cam = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(1).build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cam));
-            mMap.addMarker(new MarkerOptions().position(new LatLng(latitu, longitu)).title("Marker"));
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Marker"));
         }
         else {
-            CameraPosition cam = new CameraPosition.Builder().target(new LatLng(17.3845, 69.457 )).zoom(1).build();
+            CameraPosition cam = new CameraPosition.Builder().target(new LatLng(20.3845, 75.457 )).zoom(1).build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cam));
-            mMap.addMarker(new MarkerOptions().position(new LatLng(17.3845, 69.457)).title("Marker"));
+            mMap.addMarker(new MarkerOptions().position(new LatLng(20.3845, 75.457)).title("Marker"));
 
         }
         /**  mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
@@ -83,6 +158,7 @@ public class MapsActivity extends FragmentActivity implements View.OnClickListen
     public Thread load = new Thread() {
 
         public void run() {
+
 
             /*locm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Criteria cri = new Criteria();
@@ -104,8 +180,5 @@ public class MapsActivity extends FragmentActivity implements View.OnClickListen
         };
 
 
-    @Override
-    public void onClick(View v) {
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-    }
+
 }
